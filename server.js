13fd3fe09d370a91894e8da7067fd8b07b344370
Cameron Bass <cam@collectiveidea.com>
2017-10-30 22:47:27 -0400

    add stuff

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: "Meet Mom for lunch",
+  completed: false
+}, {
+  id: 2,
+  description: "Go to Market",
+  completed: false
+}];
 
 app.get("/", function(req, res) {
   res.send('Todo API Root');
+});
+
+app.get("/todos", function(req, res) {
+  res.json(todos);
+});
+
+app.get("/todos/:id", function(req, res) {
+  var todoId = parseInt(req.params.id);
+  var matchedTodo;
+
+  todos.forEach(function (todo) {
+    if (todoId === todo.id) {
+      matchedTodo = todo;
+    }
+  });
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.listen(PORT, function() {
